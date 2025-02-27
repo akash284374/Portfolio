4195763c1bc725dbbeca027e5a0d91c5cdfd01e1
Akash <akash284374@gmail.com>
2025-02-27 16:03:20 +0530

    my portfolio

---
+++ src/data/resume.tsx
@@ -0,0 +1,155 @@
+import { Icons } from "@/components/icons";
+import { HomeIcon, NotebookIcon } from "lucide-react";
+
+export const DATA = {
+  name: "Akash Kumar Sah",
+  initials: "AKS",
+  url: "https://LOCALHOST:3000",
+  location: "Kolkata ,West Bengal ,India",
+  locationLink: "https://www.google.com/maps/place/Kolkata,+West+Bengal",
+  description:
+    "I'm a passionate full-stack developer with expertise in HTML, CSS, JavaScript, React, and MongoDB. I love building responsive, user-friendly websites and applications that solve real-world problems. My work focuses on clean code, modern design, and efficient performance.",
+  summary:
+    "Passionate about building dynamic, user-friendly web applications with HTML, CSS, JavaScript, React, and MongoDB. I focus on clean code, modern UI/UX, and efficient performance to create seamless digital experiences.",
+  avatarUrl: "/me.png",
+  skills: [
+    "React",
+    "Next.js",
+    "Typescript",
+    "Node.js",
+    "Python",
+    "Java",
+    "C++",
+  ],
+  navbar: [
+    { href: "/", icon: HomeIcon, label: "Home" },
+    // { href: "/blog", icon: NotebookIcon, label: "Blog" },
+  ],
+  contact: {
+    email: "akash284374@gmail.com",
+    tel: "7050471987",
+    social: {
+      GitHub: {
+        name: "GitHub",
+        url: "https://github.com/akash284374",
+        icon: Icons.github,
+
+        navbar: true,
+      },
+      LinkedIn: {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/akash-kumar-sah-3217842a9?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+        icon: Icons.linkedin,
+
+        navbar: true,
+      },
+      
+      // X: {
+      //   name: "X",
+      //   url: "#",
+      //   icon: Icons.x,
+
+      //   navbar: true,
+      // },
+      // Youtube: {
+      //   name: "Youtube",
+      //   url: "#",
+      //   icon: Icons.youtube,
+      //   navbar: true,
+      // },
+
+      email: {
+        name: "Send Email",
+        url: "#",
+        icon: Icons.email,
+
+        navbar: false,
+      },
+    },
+  },
+
+  work: [
+    {
+      company: "Atomic Finance",
+      href: "https://atomic.finance",
+      badges: [],
+      location: "Remote",
+      title: "Bitcoin Protocol Engineer",
+      logoUrl: "/atomic.png",
+      start: "May 2021",
+      end: "Oct 2022",
+      description:
+        "Implemented the Bitcoin discreet log contract (DLC) protocol specifications as an open source Typescript SDK. Dockerized all microservices and setup production kubernetes cluster. Architected a data lake using AWS S3 and Athena for historical backtesting of bitcoin trading strategies. Built a mobile app using react native and typescript.",
+    },
+,
+  ],
+  education: [
+    {
+      school: "Institute of Engineering And Management ,Kolkata",
+      href: "#",
+      degree: "B.Tech",
+      logoUrl: "/buildspace.jpg",
+      start: "2022",
+      end: "2026",
+    },
+    {
+      school: "",
+      href: "https://uwaterloo.ca",
+      degree: "Bachelor's Degree of Computer Science (BCS)",
+      logoUrl: "/waterloo.png",
+      start: "2016",
+      end: "2021",
+    },
+    {
+      school: "Wilfrid Laurier University",
+      href: "https://wlu.ca",
+      degree: "Bachelor's Degree of Business Administration (BBA)",
+      logoUrl: "/laurier.png",
+      start: "2016",
+      end: "2021",
+    },
+  ],
+  projects: [
+    {
+      title: "Chat Collect",
+      href: "https://chatcollect.com",
+      dates: "Jan 2024 - Feb 2024",
+      active: true,
+      description:
+        "With the release of the [OpenAI GPT Store](https://openai.com/blog/introducing-the-gpt-store), I decided to build a SaaS which allows users to collect email addresses from their GPT users. This is a great way to build an audience and monetize your GPT API usage.",
+      technologies: [
+        "Next.js",
+        "Typescript",
+        "PostgreSQL",
+        "Prisma",
+        "TailwindCSS",
+        "Stripe",
+        "Shadcn UI",
+        "Magic UI",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://chatcollect.com",
+          icon: <Icons.globe className="size-3" />,
+        },
+      ],
+      image: "",
+      video:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/chat-collect.mp4",
+    },
+  ],
+  hackathons: [
+    {
+      title: "Hack Western 5",
+      dates: "November 23rd - 25th, 2018",
+      location: "London, Ontario",
+      description:
+        "Developed a mobile application which delivered bedtime stories to children using augmented reality.",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/hack-western.png",
+      mlh: "https://s3.amazonaws.com/logged-assets/trust-badge/2019/mlh-trust-badge-2019-white.svg",
+      links: [],
+    },
+  ],
+} as const;
